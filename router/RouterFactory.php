4ff470ed60528872a5526910124e16e8c47ce1e8
MacMini2 <djsucho@gmail.com>
2016-03-07 13:34:03 +0100

    Edit core routerFactory

--- router/RouterFactory.php
+++ router/RouterFactory.php
@@ -8,22 +8,49 @@ use Nette\Application\Routers\Route;
 
 class RouterFactory
 {
+	/** @var \Nette\DI\Container */
+	private $container;
+	
+	public function __construct(\Nette\DI\Container $container)
+	{
+		$this->container = $container;
+	}
+	
 	/**
 	 * @return Nette\Application\IRouter
 	 */
-	public static function createRouter(\h4kuna\Gettext\GettextSetup $translator)
+	public function createRouter(\h4kuna\Gettext\GettextSetup $translator)
 	{
 		$router = new RouteList();
 		
 		$router[] = new Route('index.php', 'Homepage:Homepage:default', Route::ONE_WAY);
 		
-		$router[] = new Route('[<lang ' . $translator->routerAccept() . '>/][<module>/]<presenter>/<action>/[<id>/]', [
-			'module' => 'Homepage',
-			'presenter' => 'Homepage',
-			'action' => 'default',
-			'id' => 'null',
-			'lang' => $translator->getDefault()
-		]);
+		if ($this->container->parameters['consoleMode']) {
+			$router[] = new CliRouter(array('lang' => 'en', 'action' => 'ImportExport:default'));
+			
+			$router[] = new Route('<lang>/<presenter>/<action>[/<id>][/<alias>]', [
+				'lang' => 'en',
+				'presenter' => 'Homepage',
+				'action' => 'default',
+				'id' => null
+			]);
+		} else {
+			$router[] = new Route('admin/<lang ' . $translator->routerAccept() . '>/<presenter>/<action>/<id>', [
+				'lang' => $translator->getDefault(),
+				'module' => 'Admin',
+				'presenter' => 'Dashboard',
+				'action' => 'default',
+				'id' => null
+			]);
+			
+			$router[] = new Route('[<lang ' . $translator->routerAccept() . '>/][<module>/]<presenter>/<action>/[<id>/]', [
+				'lang' => $translator->getDefault(),
+				'module' => 'Homepage',
+				'presenter' => 'Homepage',
+				'action' => 'default',
+				'id' => null
+			]);
+		}
 
 		return $router;
 	}
