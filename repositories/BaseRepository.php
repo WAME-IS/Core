8d5aa82cd5dd56117db0b17d41e8c986994698d7
shadd0w <rene.gmitter@wame.sk>
2016-04-18 09:52:32 +0200

    Fix CategoryModule

--- repositories/BaseRepository.php
+++ repositories/BaseRepository.php
@@ -7,7 +7,7 @@ use Kdyby\Doctrine\EntityManager;
 use h4kuna\Gettext\GettextSetup;
 use Nette\Security\User;
 
-class BaseRepository extends \Nette\Object
+class BaseRepository extends \Nette\Object implements \Kdyby\Persistence\Queryable
 {
 	/** @var Container */
 	public $container;
@@ -20,17 +20,26 @@ class BaseRepository extends \Nette\Object
 
 	/** @var User */
 	public $user;
+	
+	/** @var string */
+	private $name;
+	
+	/** @var \Kdyby\Doctrine\EntityRepository */
+	private $repo;
 
 	public function __construct(
 		Container $container, 
 		EntityManager $entityManager,
 		GettextSetup $translator,
-		User $user
+		User $user,
+		$name = null
 	) {
 		$this->container = $container;
 		$this->entityManager = $entityManager;
 		$this->lang = $translator->getLanguage();
 		$this->user = $user;
+		
+		$this->name = $name;
 	}
 
 	/**
@@ -55,9 +64,42 @@ class BaseRepository extends \Nette\Object
 		
 		return $reflect->getShortName();
 	}
+	
+	protected function getRepo() {
+		if (!$this->repo) {
+//			$entityBuilder = new EntityBuilder($this->name);
+//			$class = $entityBuilder->getClass();
+			
+//			$class = $this->entityHandler->getBuilder($this->name)->getClass();
+			$this->repo = $this->entityManager->getRepository('Wame\CategoryModule\Entities\CategoryEntity');
+		}
+		return $this->repo;
+	}
 
 	public function select($alias = NULL) {
-		return $alias;
+//		dump($this->getRepo());
+//		exit;
+		
+		$this->getRepo()->select($alias);
+	}
+	
+	public function createNativeQuery($sql, \Doctrine\ORM\Query\ResultSetMapping $rsm) {
+		$this->getRepo()->createNativeQuery($sql, $rsm);
+	}
+
+	public function createQuery($dql = NULL) {
+		$this->getRepo()->createQuery($dql);
+	}
+
+	public function createQueryBuilder($alias = NULL, $indexBy = NULL) {
+		$this->getRepo()->createQueryBuilder($alias, $indexBy);
+	}
+	
+	
+	public function find($id) {
+		$criteria = ['id' => $id];
+		
+		return $this->getRepo()->findOneBy($criteria);
 	}
 
 }
