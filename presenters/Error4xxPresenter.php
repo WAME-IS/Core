32fd395f0158cd05faddb5d43c07fb0cad57a833
Linzee <d.gmiterko@gmail.com>
2016-06-18 23:48:30 +0200

    SearchModule

--- presenters/Error4xxPresenter.php
+++ presenters/Error4xxPresenter.php
@@ -2,23 +2,22 @@
 
 namespace App\Presenters;
 
-use App\Core\Presenters\BasePresenter;
-use Nette\Application\BadRequestException;
-use Nette\Application\Request;
+use App\Core\Presenters\BasePresenter,
+	Nette;
 
-class Error4xxPresenter extends BasePresenter
-{
+
+class Error4xxPresenter extends BasePresenter {
 
 	public function startup()
 	{
 		parent::startup();
-		if (!$this->getRequest()->isMethod(Request::FORWARD)) {
+		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
 			$this->error();
 		}
 	}
 
 
-	public function renderDefault(BadRequestException $exception)
+	public function renderDefault(Nette\Application\BadRequestException $exception)
 	{
 		// load template 403.latte or 404.latte or ... 4xx.latte
 		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
