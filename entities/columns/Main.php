e7d158c9a8837cb97a28dd7c06962de669e37f56
shadd0w <rene.gmitter@wame.sk>
2016-08-17 14:32:07 +0200

    ADD Main entity trait

---
+++ entities/columns/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wame\Core\Entities\Columns;
+
+trait Main
+{
+    /**
+	 * @ORM\Column(name="main", type="boolean")
+	 */
+    protected $main = 0;
+
+	
+	/** get ************************************************************/
+
+	public function getMain()
+	{
+		return $this->main;
+	}
+
+
+	/** set ************************************************************/
+
+	public function setMain($main)
+	{
+		$this->main = $main;
+		
+		return $this;
+	}
+	
+}
