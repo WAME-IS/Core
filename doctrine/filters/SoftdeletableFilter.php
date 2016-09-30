0be8c80dc32371b160c10faff743489969453ce2
Stanislav Červeňák <stano@wame.sk>
2016-09-30 13:24:11 +0200

    EDIT softDeletableStatus in components and positions

--- doctrine/filters/SoftdeletableFilter.php
+++ doctrine/filters/SoftdeletableFilter.php
@@ -3,20 +3,47 @@
 namespace Wame\Core\Doctrine\Filters;
 
 use Doctrine\ORM\Mapping\ClassMetadata;
-use Zenify\DoctrineFilters\Contract\FilterInterface;
+use Zenify\DoctrineFilters\Contract\ConditionalFilterInterface;
 
-final class SoftdeletableFilter implements FilterInterface
+
+final class SoftdeletableFilter implements ConditionalFilterInterface
 {
+    /** @var boolean */
+    private $enabled = true;
+    
+    
     /**
      * {@inheritdoc}
      */
     public function addFilterConstraint(ClassMetadata $entity, $alias)
     {
-        if($entity->getReflectionClass()->hasProperty('status')) {
-            return "$alias.status != 0";
+        if ($entity->getReflectionClass()->hasProperty('status') && $this->enabled) {
+            return sprintf('%s.status != %s', $alias, 0);
         }
         
         return "";
+    }
+
+    
+    public function setEnabled($status)
+    {
+        $this->enabled = $status;
+        
+        return $this;
+    }
+    
+    
+    public function isDisabled()
+    {
+        $this->enabled = false;
+        
+        return $this;
+    }
+    
+    
+    public function isEnabled() 
+    {
+        return $this->enabled;
     }
     
 }
