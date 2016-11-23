2f2fa4b40b6a0a9e6c49ce344e149cfcbf3b19c2
Stanislav Červeňák <stano@wame.sk>
2016-11-23 09:05:16 +0100

    EDIT old forms - add dictionary

--- forms/FormFactory.php
+++ forms/FormFactory.php
@@ -10,16 +10,16 @@ class FormFactory
 {
 	/** @var int */
 	public $id;
-	
+
 	/** @var array */
 	public $formContainers = [];
-	
+
 	/** @var array */
 	public $removeFormContainers = [];
-	
+
 	/** @var string */
 	public $actionForm;
-	
+
     public function __construct()
     {
         
@@ -27,39 +27,39 @@ class FormFactory
 
 	/**
 	 * Create Form
-	 * 
+	 *
 	 * @return Form
 	 */
 	public function createForm()
 	{
 		$form = new Form;
-        
+
         $form->setParent();
-        
+
 		$form->setRenderer(new \Tomaj\Form\Renderer\BootstrapVerticalRenderer);
 //		$form->setRenderer(new \Wame\Core\Models\MaterialDesignRenderer);
 
 		$this->getActionForm($form);
-		
+
 		$this->attachFormContainers($form);
-		
+
 		return $form;
 	}
-	
+
 	/**
 	 * Attach for callbacks
-	 * 
+	 *
 	 * @param type $object
 	 */
-	protected function attached($object) 
+	protected function attached($object)
     {
         parent::attached($object);
     }
-	
-	
+
+
 	/**
 	 * Add container to form
-	 * 
+	 *
 	 * @param object $service
 	 * @param string $name
 	 * @param int $priority
@@ -72,25 +72,25 @@ class FormFactory
 			'priority' => $priority,
 			'service' => $service
 		];
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Remove form container
-	 * 
+	 *
 	 * @param string $name
 	 * @return \Wame\Core\Forms\FormFactory
 	 */
 	public function removeFormContainer($name)
 	{
 		$this->removeFormContainers[$name] = $name;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Remove form containers from array
 	 */
@@ -103,36 +103,36 @@ class FormFactory
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Add form containers
-	 * 
+	 *
 	 * @param array $containers
 	 * @return array
 	 */
 	public function addFormContainers($containers)
 	{
 		$this->formContainers = array_merge($containers, $this->formContainers);
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Return form containers
-	 * 
+	 *
 	 * @return array
 	 */
 	public function getFormContainers()
 	{
 		return $this->formContainers;
 	}
-	
-	
+
+
 	/**
 	 * Get form container
-	 * 
+	 *
 	 * @param string $name
 	 * @return Container
 	 */
@@ -140,11 +140,11 @@ class FormFactory
 	{
 		return $this->getFormContainers()[$name];
 	}
-	
-	
+
+
 	/**
 	 * Sort form containers by priority
-	 * 
+	 *
 	 * @return array
 	 */
 	public function sortFormContainers()
@@ -152,7 +152,7 @@ class FormFactory
 		$this->removeFormContainers();
 
 		$formContainers = [];
-		
+
 		foreach ($this->formContainers as $container)
 		{
 			$formContainers[$container['priority']][] = [
@@ -160,17 +160,17 @@ class FormFactory
 				'service' => $container['service']
 			];
 		}
-		
+
 		// Sort by priority
 		krsort($formContainers);
-		
+
 		return $formContainers;
 	}
-	
+
 
 	/**
 	 * Attach form containers
-	 * 
+	 *
 	 * @param Form $form
 	 * @return Form
 	 */
@@ -181,27 +181,27 @@ class FormFactory
 				$form->addComponent($container['service'], $container['name']);
 			}
 		}
-		
+
 		return $form;
 	}
-	
-	
+
+
 	/**
 	 * Set action form
-	 * 
+	 *
 	 * @param string $action
 	 */
 	public function setActionForm($action)
 	{
 		$this->actionForm = $action;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Get action form
-	 * 
+	 *
 	 * @param Form $form
 	 * @return Form
 	 * @throws \Exception
@@ -211,25 +211,25 @@ class FormFactory
 		if ($this->actionForm) {
 			$form->setAction($this->actionForm);
 		}
-		
+
 		return $form;
 	}
-	
-	
+
+
 	/**
 	 * Set database identifier
-	 * 
+	 *
 	 * @param int $id
 	 * @return \Wame\Core\Forms\FormFactory
 	 */
 	public function setId($id)
 	{
 		$this->id = $id;
-		
+
 		return $this;
 	}
-	
-	
+
+
 	/**
 	 * Set default values in form containers
 	 */
