d3a23bd3dc7d6cfcb2ed2e942cc42249aa21608a
shaddow <lameruza@gmail.com>
2016-12-28 08:58:35 +0100

    UPDATE tests

---
+++ tests/IntegrationTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wame\Core\Tests;
+
+use Tester\TestCase;
+
+abstract class IntegrationTestCase extends TestCase
+{
+    public function __construct()
+    {
+
+    }
+
+    public function getService($type)
+    {
+
+    }
+
+    public function getContainer()
+    {
+
+    }
+
+    public function createContainer()
+    {
+
+    }
+
+}
