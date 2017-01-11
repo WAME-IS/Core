5398bdf5533b4e0491b1f9ea20d23c7b11e173c8
Rene Gmitter <lameruza@gmail.com>
2017-01-11 17:04:13 +0100

    UPDATE Settings FIX Nette 2.4 compatibility UPDATE phpdoc

--- entities/BaseEntity.php
+++ entities/BaseEntity.php
@@ -3,22 +3,27 @@
 namespace Wame\Core\Entities;
 
 use Doctrine\ORM\Mapping as ORM;
+use Nette\Utils\ObjectMixin;
 
 /**
  * @ORM\MappedSuperclass
  */
 abstract class BaseEntity extends \Kdyby\Doctrine\Entities\BaseEntity
 {
-    public function __call($name, $args) 
+    /**
+     * {@inheritDoc}
+     *
+     * @deprecated
+     */
+    public function __call($name, $args) //TODO nechavame to tu? Co je toto za fix?
     {
-        //TODO nechavame to tu? Co je toto za fix?
-        if ($cb = static::extensionMethod($name)) {
-			/** @var \Nette\Callback $cb */
+        /** @var Callback $cb */
+        if ($cb = ObjectMixin::getExtensionMethod(self::class, $name)) {
 			array_unshift($args, $this);
 
 			return call_user_func_array($cb, $args);
 		}
-        
+
         return parent::__call($name, $args);
     }
     
